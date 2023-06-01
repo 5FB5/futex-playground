12a36039ff38a9a3088ed369c5b7930e7dd31991
vzubko <vecktor911@gmail.com>
2023-06-01 13:58:29 +0300

    Deleted static cause its using for no reason

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@
 
 int *myTestNumber = new int(0);
 
-static int futex(int *uaddr, int futex_op, int val, const struct timespec *timeout, int *uaddr2, int val3)
+int futex(int *uaddr, int futex_op, int val, const struct timespec *timeout, int *uaddr2, int val3)
 {
     return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr, val3);
 }
